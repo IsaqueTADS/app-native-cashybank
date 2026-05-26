5f910d6a7928263d3cbdfc8d661bf48ccb29139b
IsaqueTADS <isaqueofc18@gmail.com>
2026-05-26 16:23:58 -0300

    feat: implement DismissKeyboardView component and update login screen layout

--- src/app/(public)/login.tsx
+++ src/app/(public)/login.tsx
@@ -1,14 +1,15 @@
+import { DismissKeyboardView } from "@/components/DismissKeyBoardView";
 import { router } from "expo-router";
 import { Button, Text, TextInput, View } from "react-native";
 
 export default function Login (){
-  return <View className="flex-1 bg-green-400">
-    <Text className="text-nyc-text-primary" >
+  return <DismissKeyboardView>
+    <Text className="text-nyc-text-primary " >
       Tela de login
     </Text>
     <View className="" >
-      <TextInput className="bg-transparent w-full border border-nyc-border " />
+      <TextInput className="bg-nyc-card w-full border border-nyc-border " />
     </View>
     <Button title="sair"  onPress={() => router.navigate("/")}/>
-  </View>
+  </DismissKeyboardView>
 }
---
+++ src/components/DismissKeyBoardView/index.tsx
@@ -0,0 +1,14 @@
+import { Keyboard, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export function DismissKeyboardView ({children}: React.PropsWithChildren) {
+  return <SafeAreaView className="flex-1 bg-nyc-bg">
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView behavior="padding" className="flex-1">
+        <ScrollView >
+          {children}
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  </SafeAreaView>
+}
