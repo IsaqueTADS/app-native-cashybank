cd37e4741f34d40a6bd3008a6708644081d73467
IsaqueTADS <isaqueofc18@gmail.com>
2026-05-30 14:27:01 -0300

    refactor(Error): add icon to Error component

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,5 +1,18 @@
-import { Text } from 'react-native'
+import { useTheme } from '@/hooks/use-theme'
+import { MaterialIcons } from '@expo/vector-icons'
+import { Text, View } from 'react-native'
 
 export const Error = ({ children }: React.PropsWithChildren) => {
-  return <Text className="text-destructive">{children}</Text>
+  const theme = useTheme()
+  return (
+    <View className="flex-row items-center ">
+      <MaterialIcons
+        name="error-outline"
+        size={16}
+        color={theme.colors.destructive}
+        className="mr-1"
+      />
+      <Text className="text-destructive">{children}</Text>
+    </View>
+  )
 }
