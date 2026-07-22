641572db760d5bdb02e0be073f82fe02642740bf
IsaqueTADS <isaqueofc18@gmail.com>
2026-07-22 16:01:13 -0300

    feat: enhance Snackbar component with animations for visibility and message display

--- src/components/Snackbar/index.tsx
+++ src/components/Snackbar/index.tsx
@@ -1,16 +1,41 @@
 import { useSnackbarContext } from '@/contexts/snackbar-context'
-import { Text, View } from 'react-native'
+import { useEffect } from 'react'
+import { Text } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
 
 export function Snackbar() {
   const { visible, message } = useSnackbarContext()
 
+  const translateY = useSharedValue(100)
+  const opacity = useSharedValue(0)
+
+  useEffect(() => {
+    if (visible) {
+      opacity.value = withTiming(1, { duration: 300 })
+      translateY.value = withTiming(0, { duration: 400 })
+    } else {
+      opacity.value = withTiming(0, { duration: 200 })
+      translateY.value = withTiming(100, { duration: 300 })
+    }
+  }, [visible, opacity, translateY])
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+    opacity: opacity.value,
+  }))
+
   if (!visible) return <></>
 
   return (
-    <View
-      className={`h-{50px } absolute bottom-10 z-10 flex h-[50px] w-[90%] items-center justify-center self-center rounded-xl bg-destructive p-2`}
+    <Animated.View
+      style={animatedStyle}
+      className="absolute bottom-10 z-10 flex h-[50px] w-[90%] items-center justify-center self-center rounded-xl bg-destructive p-2"
     >
       <Text className="text-bold text-base text-white">{message}</Text>
-    </View>
+    </Animated.View>
   )
 }
